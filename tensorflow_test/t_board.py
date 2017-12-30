0d3e2054fc5c60228c52210c3d06940bb671e261
meetyyj <meetyyj@gmail.com>
2017-12-30 20:34:07 +0800

    tensor board

---
+++ tensorflow_test/t_board.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+## tensorboard --logdir='logs/'
+def add_layer(input, in_size, out_size, activation_func=None):
+    with tf.name_scope('layer'):
+        with tf.name_scope('weights'):
+            Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
+        with tf.name_scope('bias'):
+            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')                # not zero
+        with tf.name_scope('Wx_b'):
+            Wx_plus_b = tf.matmul(input, Weights) + biases
+    if activation_func is None:
+        output = Wx_plus_b
+    else:
+        output = activation_func(Wx_plus_b)
+
+    return output
+
+x_data = np.linspace(-1, 1, 300)[:, np.newaxis]    # 300 x 1
+noise = np.random.normal(0, 0.05, x_data.shape)
+y_data = np.square(x_data) - 0.5 + noise
+
+with tf.name_scope('input'):
+    xs = tf.placeholder(tf.float32, [None, 1], name="x_input")
+    ys = tf.placeholder(tf.float32, [None, 1], name="y_input")
+
+
+# in_size is the feature size
+l1 = add_layer(xs, 1, 10, activation_func=tf.nn.relu)
+prediction = add_layer(l1, 10, 1, activation_func=None)
+
+with tf.name_scope('loss'):
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
+                                reduction_indices=[1]))
+with tf.name_scope('train_step'):
+    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+init = tf.global_variables_initializer()
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.scatter(x_data, y_data)
+plt.ion()
+plt.show()
+
+with tf.Session() as sess:
+    sess.run(init)
+    writer = tf.summary.FileWriter("logs/", sess.graph)
+    for i in range(1000):
+        sess.run(train_step, feed_dict={xs:x_data, ys:y_data})
+        if i % 50 == 0:
+            print(sess.run(loss, feed_dict={xs:x_data, ys:y_data}))
--- tensorflow_test/t_layer.py
+++ tensorflow_test/t_layer.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 def add_layer(input, in_size, out_size, activation_func=None):
     Weights = tf.Variable(tf.random_normal([in_size, out_size]))
@@ -19,6 +20,7 @@ y_data = np.square(x_data) - 0.5 + noise
 xs = tf.placeholder(tf.float32, [None, 1])
 ys = tf.placeholder(tf.float32, [None, 1])
 
+# in_size is the feature size
 l1 = add_layer(xs, 1, 10, activation_func=tf.nn.relu)
 prediction = add_layer(l1, 10, 1, activation_func=None)
 
@@ -27,11 +29,25 @@ loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
 train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 init = tf.global_variables_initializer()
 
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.scatter(x_data, y_data)
+plt.ion()
+plt.show()
+
 with tf.Session() as sess:
     sess.run(init)
     for i in range(1000):
         sess.run(train_step, feed_dict={xs:x_data, ys:y_data})
         if i % 50 == 0:
             print(sess.run(loss, feed_dict={xs:x_data, ys:y_data}))
-
+            try:
+                ax.lines.remove(lines[0])
+            except Exception:
+                pass
+            # useless
+            prediction_value = sess.run(prediction, feed_dict={xs:x_data})
+            lines = ax.plot(x_data, prediction_value, 'r-', lw=3)
+            # print(lines)
+            plt.pause(0.1)
 
