a958e994d71676c356cc041844289354c337baca
meetyyj <meetyyj@gmail.com>
2017-12-30 15:50:21 +0800

    test layer

---
+++ tensorflow_test/t_layer.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+import numpy as np
+
+def add_layer(input, in_size, out_size, activation_func=None):
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)                # not zero
+    Wx_plus_b = tf.matmul(input, Weights) + biases
+    if activation_func is None:
+        output = Wx_plus_b
+    else:
+        output = activation_func(Wx_plus_b)
+
+    return output
+
+x_data = np.linspace(-1, 1, 300)[:, np.newaxis]    # 300 x 1
+noise = np.random.normal(0, 0.05, x_data.shape)
+y_data = np.square(x_data) - 0.5 + noise
+
+xs = tf.placeholder(tf.float32, [None, 1])
+ys = tf.placeholder(tf.float32, [None, 1])
+
+l1 = add_layer(xs, 1, 10, activation_func=tf.nn.relu)
+prediction = add_layer(l1, 10, 1, activation_func=None)
+
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
+                                    reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    for i in range(1000):
+        sess.run(train_step, feed_dict={xs:x_data, ys:y_data})
+        if i % 50 == 0:
+            print(sess.run(loss, feed_dict={xs:x_data, ys:y_data}))
+
+
