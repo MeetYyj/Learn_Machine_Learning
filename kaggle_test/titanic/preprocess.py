ed87c9df6a1f40885221f0d36897a1b2e61a2854
meetyyj <meetyyj@gmail.com>
2018-01-04 20:28:27 +0800

    data preprocessing

---
+++ kaggle_test/titanic/preprocess.py
@@ -0,0 +1,63 @@
+
+import pandas as pd
+
+from sklearn.ensemble import RandomForestRegressor
+import sklearn.preprocessing as preprocessing
+
+# 使用 RandomForestClassifier 填补缺失的年龄属性
+def set_missing_ages(df):
+
+    # 把已有的数值型特征取出来丢进Random Forest Regressor中
+    age_df = df[['Age','Fare', 'Parch', 'SibSp', 'Pclass']]
+
+    # 乘客分成已知年龄和未知年龄两部分
+    known_age = age_df[age_df.Age.notnull()].as_matrix()
+    unknown_age = age_df[age_df.Age.isnull()].as_matrix()
+
+    # y即目标年龄
+    y = known_age[:, 0]
+
+    # X即特征属性值
+    X = known_age[:, 1:]
+
+    # fit到RandomForestRegressor之中
+    rfr = RandomForestRegressor(random_state=0, n_estimators=2000, n_jobs=-1)
+    rfr.fit(X, y)
+
+    # 用得到的模型进行未知年龄结果预测
+    predictedAges = rfr.predict(unknown_age[:, 1::])
+
+    # 用得到的预测结果填补原缺失数据
+    df.loc[ (df.Age.isnull()), 'Age' ] = predictedAges
+
+    return df, rfr
+
+def set_Cabin_type(df):
+    df.loc[ (df.Cabin.notnull()), 'Cabin' ] = "Yes"
+    df.loc[ (df.Cabin.isnull()), 'Cabin' ] = "No"
+    return df
+
+data_train = pd.read_csv("/home/yyj/Learn_Machine_Learning/kaggle_test/titanic/train.csv")
+data_train, rfr = set_missing_ages(data_train)
+data_train = set_Cabin_type(data_train)
+
+# 特征因子化
+dummies_Cabin = pd.get_dummies(data_train['Cabin'], prefix= 'Cabin')
+
+dummies_Embarked = pd.get_dummies(data_train['Embarked'], prefix= 'Embarked')
+
+dummies_Sex = pd.get_dummies(data_train['Sex'], prefix= 'Sex')
+
+dummies_Pclass = pd.get_dummies(data_train['Pclass'], prefix= 'Pclass')
+
+df = pd.concat([data_train, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1) # 加入刚刚因子化的特征
+df.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True) #丢弃没用的特征
+
+# 归一化
+scaler = preprocessing.StandardScaler()
+age_scale_param = scaler.fit(df['Age'].values.reshape(-1, 1))
+df['Age_scaled'] = scaler.fit_transform(df['Age'].values.reshape(-1, 1), age_scale_param)
+fare_scale_param = scaler.fit(df['Fare'].values.reshape(-1, 1))
+df['Fare_scaled'] = scaler.fit_transform(df['Fare'].values.reshape(-1, 1), fare_scale_param)
+
+df.to_csv("/home/yyj/Learn_Machine_Learning/kaggle_test/titanic/processed.csv", index=False)
